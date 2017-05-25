da124c81f46653b1fc78536d5f6eb4c3be32ff5b
rrlero <roman@xlan.net.ua>
2017-05-25 18:08:14 +0300

    chubin created

--- add_car/create_user.py
+++ add_car/create_user.py
@@ -23,7 +23,7 @@ def get_arguments_del():
 class CreateUser(Resource):
     def get(self):
         query = session_git.query(Users)
-        users_list = [{'user_name': user.user_name, 'id': user.id} for user in query]
+        users_list = [{'user_name': user.user_name, 'id': user.id, 'password': user.user_password, 'token': user.token} for user in query]
         session_git.close()
         return jsonify(users_list)
 
@@ -40,20 +40,20 @@ class CreateUser(Resource):
         session_git.close()
         return {'message': 'new user created'}, 201
 
-    # def delete(self):
-    #     args_del = get_arguments_del()
-    #     id_user = args_del.get('id_user')
-    #     password_for_del
-    #     query = session_git.query(Users)
-    #     for user in query:
-    #         if user.id == id_user:
-    #             cars = session_git.query(Cars).join(Cars, Users.lnk_users_cars).filter(Users.id == id_user)
-    #             for car in cars:
-    #                 notes = session_git.query(Notes).join(Notes, Cars.lnk_cars_notes).filter(Cars.id == car.id)
-    #                 for note in notes:
-    #                     session_git.delete(note)
-    #                 session_git.delete(car)
-    #             session_git.delete(user)
-    #     session_git.commit()
-    #     session_git.close()
-    #     return {'message': 'user deleted'}, 201
+    def delete(self):
+        args_del = get_arguments_del()
+        id_user = args_del.get('id_user')
+        password_for_del
+        query = session_git.query(Users)
+        for user in query:
+            if user.id == id_user:
+                cars = session_git.query(Cars).join(Cars, Users.lnk_users_cars).filter(Users.id == id_user)
+                for car in cars:
+                    notes = session_git.query(Notes).join(Notes, Cars.lnk_cars_notes).filter(Cars.id == car.id)
+                    for note in notes:
+                        session_git.delete(note)
+                    session_git.delete(car)
+                session_git.delete(user)
+        session_git.commit()
+        session_git.close()
+        return {'message': 'user deleted'}, 201
