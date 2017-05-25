93d454843c84ce10dd0e2c91fdd59a903fad2d9b
rrlero <roman@xlan.net.ua>
2017-05-25 18:08:58 +0300

    chubin created

--- add_car/create_user.py
+++ add_car/create_user.py
@@ -23,7 +23,7 @@ def get_arguments_del():
 class CreateUser(Resource):
     def get(self):
         query = session_git.query(Users)
-        users_list = [{'user_name': user.user_name, 'id': user.id, 'password': user.user_password, 'token': user.token} for user in query]
+        users_list = [{'user_name': user.user_name, 'id': user.id, 'password': user.user_password, 'token': user.user_token} for user in query]
         session_git.close()
         return jsonify(users_list)
 
